26669710a03f55c8cfff6e447c9a764b4e7b0eec
Ana Paula <aninha10.desouza@gmail.com>
2022-06-15 08:28:02 -0300

    ok

---
+++ src/views/JogadoresView.vue
@@ -0,0 +1,99 @@
+<script>
+import { v4 as uuid } from "uuid";
+export default {
+  data() {
+    return {
+      novo_time: "",
+      jogadores: [
+        {
+          id: "ac803529-5e2d-45b2-a907-ca993bb301ea",
+          nome: "jogador 1",
+          time: "Time 1",
+        },
+        {
+          id: "610e1ad0-0302-492e-a686-9bfb62cc2a82",
+          nome: "jogador 2",
+          time: "Time 2",
+        },
+        {
+          id: "29eaba8d-a376-485e-9509-5dddd6587f0d",
+          nome: "jogador 3",
+          time: "Time 3",
+        },
+        {
+          id: "d7c068f0-928f-4858-9ae3-eadc32edb230",
+          nome: "jogador 4",
+          time: "Time 4",
+        },
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+      if (this.novo_jogador !== "") {
+        const novo_id = uuid();
+        this.jogadores.push({
+          id: novo_id,
+          name: this.novo_jogador,
+        });
+        this.novo_jogador = "";
+      }
+    },
+    excluir(jogador) {
+      const indice = this.jogadores.indexOf(jogador);
+      this.jogadores.splice(indice, 1);
+    },
+    alerta() {
+      alert("ok");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerencimento de Jogadores</h2>
+    </div>
+    <div class="form-input">
+      <input
+        type="text"
+        v-model="novo_time"
+        @keyup.enter="salvar"
+        placeholder="Times"
+      />
+      <input
+        type="text"
+        v-model="novo_jogador"
+        @keyup.enter="salvar"
+        placeholder="Jogadores"
+      />
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-items">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nomes</td>
+            <td>Time</td>
+            <td>Ações</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="jogador in jogadores" :key="jogador.id">
+            <td>{{ jogador.id }}</td>
+            <td>{{ jogador.name }}</td>
+            <td>{{ jogador.time }}</td>
+            <td>
+              <button @click="alerta">Editar</button>
+              <button @click="excluir">excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/TimesView.vue
@@ -0,0 +1,71 @@
+<script>
+import { v4 as uuid } from "uuid";
+export default {
+  data() {
+    return {
+      novo_time: "",
+      times: [
+        { id: "f12bc7ce-5ca7-4cbf-ac6e-be3e59cb2862", name: "Time 1" },
+        { id: "b880659d-5d33-4607-ae19-8a22a738b509", name: "Time 2" },
+        { id: "0f97fea6-72ac-4ee0-bf1b-92516d3417e1", name: "Time 3" },
+        { id: "25c441be-e90a-4847-98a7-0d888e4c981f", name: "Time 4" },
+      ],
+    };
+  },
+  methods: {
+    salvar() {
+      // const novo_id = uuid();
+      this.times.push({
+        id: "bbf13e16-8a92-49da-b37b-9f1f218bc384",
+        name: this.novo_time,
+      });
+    },
+    excluir(time) {
+      const indice = this.times.indexOf(time);
+      this.times.splice(indice, 1);
+    },
+    alerta() {
+      alert("ok");
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="container">
+    <div class="title">
+      <h2>Gerencimento de Times</h2>
+    </div>
+    <div class="form-input">
+      <input
+        type="text"
+        v-model="novo_time"
+        @keyup.enter="salvar"
+        placeholder="Times"
+      />
+      <button @click="salvar">Salvar</button>
+    </div>
+    <div class="list-items">
+      <table>
+        <thead>
+          <tr>
+            <td>ID</td>
+            <td>Nomes</td>
+            <td>Ações</td>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="time in times" :key="time.id">
+            <td>{{ time.id }}</td>
+            <td>{{ time.name }}</td>
+            <td>
+              <button @click="alerta">Editar</button>
+              <button @click="excluir">excluir</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
+</template>
+<style></style>
